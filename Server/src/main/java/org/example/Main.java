f4d41a88d1c24d3c90fea6365c4c9abf85c0648b
jword <didehdrb21@naver.com>
2024-02-19 17:57:31 +0900

    shadowJar 테스트(미완)

--- Server/src/main/java/org/example/Main.java
+++ Server/src/main/java/org/example/Main.java
@@ -5,7 +5,8 @@ import java.util.*;
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         Server server = new Server();
+        server.start();
     }
 }
